2d8fff5c840a2bbf7e67a3d7ed489d4e73c34ca4
matt-sanpedro <mattsanpedro7@gmail.com>
2025-01-09 21:27:29 -0800

    Added examples of primitive data types

--- section-02-core-java/p-00-intro/Hello.java
+++ section-02-core-java/p-00-intro/Hello.java
@@ -1,6 +1,16 @@
 class Hello {
     
     public static void main(String a[]) {
-        System.out.println("Hello, World!");
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1 + num2;
+        byte by = 127;
+        short sh = 558;
+        long l = 1234567890L;
+        float f = 5.8f;
+        double d = 5.8;
+        char c = 'A';
+        boolean b = true;
+        System.out.println("TOTAL: "+ result + "\nBYTES: " + by + "\nSHORT: " + sh + "\nLONG: " + l + "\nFLOAT: " + f + "\nDOUBLE: " + d + "\nCHAR: " + c + "\nBOOLEAN: " + b);
     }
 }
