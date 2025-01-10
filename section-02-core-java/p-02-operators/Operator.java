9dbfc176278a5f57940a377ebd42cafda075f29f
matt-sanpedro <mattsanpedro7@gmail.com>
2025-01-09 23:06:39 -0800

    Added arithmetic operator examples

---
+++ section-02-core-java/p-02-operators/Operator.java
@@ -0,0 +1,16 @@
+class Operator {
+    public static void main(String[] args) {
+        // dividing two integers will give you an integer result
+        int num1 = 7;
+        int num2 = 5;
+        int result = num1 / num2;
+        System.out.println("Result: " + result);
+
+        // incrementing a number
+        num1 += 2;
+        num1++; // post-increment
+        ++num1; // pre-increment
+        num1--;
+        System.out.println("num1: " + num1);
+    }
+}
