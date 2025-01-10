161766796788aafffd23ecef3cef06b75140fe22
matt-sanpedro <mattsanpedro7@gmail.com>
2025-01-09 23:32:28 -0800

    Added logical and operator example

---
+++ section-02-core-java/p-02-operators/Logical.java
@@ -0,0 +1,12 @@
+class Logical {
+    public static void main(String[] args) {
+        // dividing two integers will give you an integer result
+        int num1 = 7;
+        int num2 = 5;
+        int a = 8;
+        int b = 11;
+    
+        boolean result = (num1 > num2) && (a < b); // use && for AND; use || for OR;
+        System.out.println(result);
+    }
+}
