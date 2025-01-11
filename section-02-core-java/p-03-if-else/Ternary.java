8dcdf6e45925c8cc2b11d7e3a5c34341e69fd444
matt-sanpedro <mattsanpedro7@gmail.com>
2025-01-11 13:35:59 -0800

    Added example on using Ternary operator on if else logic

---
+++ section-02-core-java/p-03-if-else/Ternary.java
@@ -0,0 +1,19 @@
+public class Ternary {
+
+    public static void main(String[] args) {
+        int n = 5;
+        int result = 0;
+
+        // // if else syntax
+        // if (n%2==0)
+        //     result = 10;
+        // else
+        //     result = 20;
+
+        // ternary syntax
+        result = (n%2==0) ? 10 : 20;
+
+        System.out.println(result);
+
+    }
+}
