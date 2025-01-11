2079d94ab1873ed5d8bd74b3ca96ed4338b4ef0b
matt-sanpedro <mattsanpedro7@gmail.com>
2025-01-11 13:26:39 -0800

    Added examples on if else logic

---
+++ section-02-core-java/p-03-if-else/Condition.java
@@ -0,0 +1,31 @@
+public class Condition {
+
+    public static void main(String[] args) {
+        int a = 18;
+
+        // do NOT need curly braces if there is only one statement
+        if (a > 10 && a < 20) {
+            System.out.println("a is greater than 10 and less than 20");
+            System.out.println("thank you");
+        } else {
+            System.out.println("a is out of range");
+        }
+
+        int x = 8;
+        int y = 7;
+        int z = 6;
+
+        if (x>y && x>z) {
+            System.out.println(x);
+            System.out.println("x is greater than y and z");
+        }
+        else if(y>z){
+            System.out.println(y);
+            System.out.println("y is greater than x and z");
+        }
+        else {
+            System.out.println(z);
+        }
+
+    }
+}
