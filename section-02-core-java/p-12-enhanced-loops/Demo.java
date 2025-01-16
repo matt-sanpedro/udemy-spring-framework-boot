c1d0e95bbcedd4a5e8e9319cc14f5683bfabab1c
matt-sanpedro <mattsanpedro7@gmail.com>
2025-01-16 00:09:07 -0800

    Init demo for enhanced loops

---
+++ section-02-core-java/p-12-enhanced-loops/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+
+    public static void main(String[] args) {
+        int nums[] = new int[4];
+        nums[0] = 4;
+        nums[1] = 8;
+        nums[2] = 3;
+        nums[3] = 9;
+
+    }
+}
