311783ee0ef9d49259fb982b67c80ac8557acb87
matt-sanpedro <mattsanpedro7@gmail.com>
2025-01-21 17:53:03 -0800

    Added example on enhanced for loop implemented on the list of objects of Students

--- section-02-core-java/p-12-enhanced-loops/Demo.java
+++ section-02-core-java/p-12-enhanced-loops/Demo.java
@@ -1,3 +1,9 @@
+class Student {
+    int rollno;
+    String name;
+    int marks;
+}
+
 public class Demo {
 
     public static void main(String[] args) {
@@ -7,5 +13,40 @@ public class Demo {
         nums[2] = 3;
         nums[3] = 9;
 
+        // // traditional for loop
+        // for(int i=0; i<nums.length; i++) {
+        //     System.out.println(nums[i]);
+        // }
+
+        // enhanced for loop
+        for(int n : nums) {
+            System.out.println(n);
+        }
+
+        // create an object of Student class
+        Student s1 = new Student();
+        s1.rollno = 101;
+        s1.name = "John";
+        s1.marks = 84;
+
+        Student s2 = new Student();
+        s2.rollno = 102;
+        s2.name = "Mark";
+        s2.marks = 87;
+
+        Student s3 = new Student();
+        s3.rollno = 103;
+        s3.name = "Matt";
+        s3.marks = 96;
+
+        // an array that can hold student objects 
+        Student students[] = new Student[3];
+        students[0] = s1;
+        students[1] = s2;
+        students[2] = s3;
+        
+        for(Student stud : students) {
+            System.out.println(stud.name + " : " + stud.marks);
+        }
     }
 }
