a3e86ce5cf11ee7a20d08f78267b5841c577cc35
matt-sanpedro <mattsanpedro7@gmail.com>
2025-04-06 21:22:18 -0700

    Added notes on the data types used

--- section-02-core-java/p-00-intro/Hello.java
+++ section-02-core-java/p-00-intro/Hello.java
@@ -1,6 +1,12 @@
+// class name should be same as file name
+// extension for byte code is .class
+// java is called a strongly typed language
+// primitive data types: int, byte, short, long, float, double, char, boolean
+// object data types: String, Integer, Byte, Short, Long, Float, Double, Character, Boolean
 class Hello {
-    
+    // object oriented: everything should be an object
     public static void main(String a[]) {
+        // type variable_name = value;
         int num1 = 3;
         int num2 = 5;
         int result = num1 + num2;
@@ -10,7 +16,7 @@ class Hello {
         float f = 5.8f;
         double d = 5.8;
         char c = 'A';
-        boolean b = true;
+        boolean b = true; // in java, no 0 or 1 for boolean
         System.out.println("TOTAL: "+ result + "\nBYTES: " + by + "\nSHORT: " + sh + "\nLONG: " + l + "\nFLOAT: " + f + "\nDOUBLE: " + d + "\nCHAR: " + c + "\nBOOLEAN: " + b);
     }
 }
