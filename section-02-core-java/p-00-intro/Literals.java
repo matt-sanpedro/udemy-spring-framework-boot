3f9af28b2a673ee0707bf66175343db650552c0f
matt-sanpedro <mattsanpedro7@gmail.com>
2025-04-06 21:27:40 -0700

    Added additional examples on literal values

--- section-02-core-java/p-00-intro/Literals.java
+++ section-02-core-java/p-00-intro/Literals.java
@@ -1,6 +1,12 @@
 class Literals {
     
     public static void main(String a[]) {
+        // can add underscores to values for easy readability
+        double d = 1_000_000.00;
+        System.out.println("DOUBLE: " + d);
+        // can use e for scientific notation
+        double num1 = 12e10;
+        System.out.println("BIG NUMBER: " + num1);
         char c = 'A';
         c++;
         System.out.println("CHAR: " + c);
