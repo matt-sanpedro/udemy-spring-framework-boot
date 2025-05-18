435cac42a8304e86e64f558b08b31f13f2436f77
matt-sanpedro <mattsanpedro7@gmail.com>
2025-05-18 12:07:26 -0700

    Corrected AllPosts tag, missing extra space from closing tag

--- section-07-rest/ReactUI/JOBPORTAL-1-FULL-CRUD-UI/src/App.js
+++ section-07-rest/ReactUI/JOBPORTAL-1-FULL-CRUD-UI/src/App.js
@@ -12,7 +12,7 @@ function App() {
    <Navbar/>
    <BrowserRouter>
    <Routes>
-   <Route path='/' element={<AllPosts/>}/>
+   <Route path='/' element={<AllPosts />}/>
    <Route path="/create" element={<Create />} />
    <Route path="/edit" element={<Edit />} />
    </Routes>
